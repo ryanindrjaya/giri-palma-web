1b0018417bbda7f00bf5413a01080860065ff4e4
Ryan Indrajaya <sryan062@gmail.com>
2023-11-14 17:45:33 +0700

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import { themeConfig } from "@/lib/theme.config";
 import "@/styles/globals.css";
 import { ConfigProvider, notification } from "antd";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [api, contextHolder] = notification.useNotification();
+
   return (
     <ConfigProvider theme={themeConfig}>
       {contextHolder}
+      <NextNProgress color="#7cb4c3" />
       <Component {...pageProps} notificationApi={api} />;
     </ConfigProvider>
   );
